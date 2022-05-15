b762e877fef9f5eafb5e79c996b1b1f7d23e0b5e
Antonio Kim <antoniok@sfu.ca>
2022-05-15 05:43:12 -0700

    Add Clarifai Module to App.js
    Only Clarifai module and onSubmit has been modified. API key has
    been removed for security purpose. The button console log does not
    work and needs to be fixed.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,46 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Clarifai from "clarifai;";
 import "./App.css";
 
+const app = new Clarifai.App({
+  apikey: "",
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onSubmit = () => {
+    App.models
+      .predict(
+        "a4032429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then({
+        function(response) {},
+        function(err) {},
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onSubmit}
+        />
       </div>
     );
   }
